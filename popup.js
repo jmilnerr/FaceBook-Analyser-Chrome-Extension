fb52f73c7aabb7f9d4a3a1b91826004a00a96b40
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2020-04-03 10:45:04 +0200

    Update popup.js

--- popup.js
+++ popup.js
@@ -12,4 +12,5 @@ function popup() {
 }
 
 //knyt en EventListener til "Tag tekst ..."-knappen
+//og eksekver pop()-funktionen ovenfor
 document.getElementById("fbGetMessage").addEventListener("click", popup);
