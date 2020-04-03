12e8d661e3d310976242fa3046cced2274a77c4d
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2020-04-03 12:38:35 +0200

    Create test_train_model.py

---
+++ test_train_model.py
@@ -0,0 +1,118 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+from numpy import array
+from numpy import asarray
+from numpy import zeros
+from numpy import array
+from keras.preprocessing.text import one_hot
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten
+from keras.layers import GlobalMaxPooling1D
+from keras.layers.embeddings import Embedding
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+import tensorflowjs as tfjs
+from keras.layers.recurrent import LSTM
+
+movie_reviews = pd.read_csv("IMDB Dataset.csv")
+movie_reviews["review"][3]
+
+
+def preprocess_text(sen):
+
+    sentence = remove_tags(sen)
+    sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+    sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)
+    sentence = re.sub(r'\s+', ' ', sentence)
+
+    return sentence
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    return TAG_RE.sub('', text)
+
+
+X = []
+sentences = list(movie_reviews['review'])
+for sen in sentences:
+    X.append(preprocess_text(sen))
+
+X[3]
+
+y = movie_reviews['sentiment']
+
+y = np.array(list(map(lambda x: 1 if x=="positive" else 0, y)))
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+
+tokenizer = Tokenizer(num_words=5000)
+tokenizer.fit_on_texts(X_train)
+
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+# Adding 1 because of reserved 0 index
+vocab_size = len(tokenizer.word_index) + 1
+
+maxlen = 100
+
+X_train = pad_sequences(X_train, padding='post', maxlen=maxlen)
+X_test = pad_sequences(X_test, padding='post', maxlen=maxlen)
+
+
+embeddings_dictionary = dict()
+glove_file = open('glove.6B.100d.txt', encoding="utf8")
+
+for line in glove_file:
+    records = line.split()
+    word = records[0]
+    vector_dimensions = asarray(records[1:], dtype='float32')
+    embeddings_dictionary[word] = vector_dimensions
+glove_file.close()
+
+embedding_matrix = zeros((vocab_size, 100))
+for word, index in tokenizer.word_index.items():
+    embedding_vector = embeddings_dictionary.get(word)
+    if embedding_vector is not None:
+        embedding_matrix[index] = embedding_vector
+
+# SELVE NN-MODELLEN
+model = Sequential()
+embedding_layer = Embedding(vocab_size, 100, weights=[embedding_matrix], input_length=maxlen , trainable=False)
+model.add(embedding_layer)
+model.add(LSTM(128))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
+
+print(model.summary())
+
+history = model.fit(X_train, y_train, batch_size=128, epochs=6, verbose=1, validation_split=0.2)
+
+score = model.evaluate(X_test, y_test, verbose=1)
+
+print("Test Score:", score[0])
+print("Test Accuracy:", score[1])
+
+instance = X[57]
+print(instance)
+
+instance = tokenizer.texts_to_sequences([instance])
+
+flat_list = []
+for sublist in instance:
+    for item in sublist:
+        flat_list.append(item)
+
+flat_list = [flat_list]
+
+instance = pad_sequences(flat_list, padding='post', maxlen=maxlen)
+
+print(model.predict(instance))
+
+tfjs.converters.save_keras_model(model, 'models')
