b42fd209de56b49b4e4daf72656a6c872babe7bf
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2020-04-03 10:48:21 +0200

    Update content.js

--- content.js
+++ content.js
@@ -1,10 +1,13 @@
-// data we are interested in
+//tag data, vi er interesserede i
 var text = document.querySelectorAll('[data-testid="post_message"]')[0].innerText
 
+//lyt efter indkommende "messages"
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
+  //vær opmærksom på getData, og om den er lig med den sendt fra popup.js
   if (request.getData == "hello") {
-    //console.log(text)
+    
+    //send resultatet tilbage til popup.js i objektet med "serveData"
     sendResponse({serveData: text});
   }
   return true;
